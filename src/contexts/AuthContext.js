7d79d826905dd4f684f8736740cbb79ba784a06e
xanf-code <darshanaswath@gmail.com>
2021-01-03 22:12:39 +0530

    Added Forget Password

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -24,6 +24,10 @@ export function AuthProvider({ children }) {
     return auth.signOut()
   }
 
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email)
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       setCurrentUser(user)
@@ -38,6 +42,7 @@ export function AuthProvider({ children }) {
     login,
     signup,
     logout,
+    resetPassword,
   }
 
   return (
